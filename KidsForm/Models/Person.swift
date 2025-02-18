1300c8c8a61cf29e3387988548b147a15f190b4c
Egor Kruglov <yegorkruglov@gmail.com>
2025-02-18 14:53:08 +0300

    + person model

---
+++ KidsForm/Models/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  KidsForm
+//
+//  Created by Egor Kruglov on 18.02.2025.
+//
+
+import Foundation
+
+struct Person: Hashable {
+    let name: String
+    let age: String
+}
