b0b7c210359a1b45dd2feeabdad79f9ab6a6a533
Egor Kruglov <yegorkruglov@gmail.com>
2025-02-18 11:40:25 +0300

    Initial Commit

---
+++ KidsForm/Modules/KidsFormViewController.swift
@@ -0,0 +1,18 @@
+//
+//  KidsFormViewController.swift
+//  KidsForm
+//
+//  Created by Egor Kruglov on 18.02.2025.
+//
+
+import UIKit
+
+final class KidsFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
